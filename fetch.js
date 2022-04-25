5ac107421fe94bbc21c98a1001ec578d444e22c8
unknown <hanhcu7777@gmail.com>
2022-04-25 20:34:38 +0700

    fetch

--- fetch.js
+++ fetch.js
@@ -1,1 +1,14 @@
-alert('');
+const ulElement = document.querySelector('user-list');
+
+const getAllUsers = async () =>{
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const result = await res.json();
+    return result;
+}
+
+const renderUsers = async() =>{
+    const users = await getAllUsers();
+    ulElement.innerHTML = users.map(user=>`<h1>${user.name}</h1>`).join('');
+}
+
+renderUsers();
